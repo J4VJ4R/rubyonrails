ea27b33bad8a6128450eff36261dde5fb845c4ad
Javier Jaramillo <ingenierodesoftware73@gmail.com>
2020-11-28 12:09:21 -0500

    create and delete controllers for a test

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
+  get 'todo/index'
+
+  get 'todo/show'
+
+  get 'todo/create'
+
+  get 'todo/edit'
+
+  get 'todo/update'
+
+  get 'todo/destroy'
+
+  resources :posts
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
